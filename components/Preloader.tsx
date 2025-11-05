145fe45e4c41bb1f6961a16cc6c3f210eeb5387f
saksham3403 <sakshamsingh.gj14@gmail.com>
2025-11-05 10:57:07 +0530

    Change text from 'HC' to 'HR' in Preloader

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -5,7 +5,7 @@ const Preloader: React.FC = () => {
     return (
         <div className="fixed inset-0 bg-[#0a0a0a] z-[9999] flex items-center justify-center animate-preloader-fade-out">
             <div className="text-white text-6xl font-playfair font-bold animate-logo-pulse">
-                HC
+                HR
             </div>
         </div>
     );
